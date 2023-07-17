b110ac1d1f22f81cad203719f7dd3a7a53222b16
Anzar Khan <67134357+benzocv@users.noreply.github.com>
2023-07-17 14:40:37 +0530

    new data added

---
+++ project/inquiry.php
@@ -0,0 +1,31 @@
+<?php
+ob_start();
+
+	
+	if(isset($_POST['submit']))
+	
+	{
+			
+			
+				   	
+					
+             $message = "Name : $_POST[name]\n Mobile No. : $_POST[phone]\n Email : $_POST[email]\n Message : $_POST[message]\n ";			
+			 
+			 $message1 = "Thank you $_POST[name] for your Message. We have received your details and shall get back to you shortly. Thank you \n ";			
+
+                    $headers = "From: <content@socialtalkers.in>";
+
+
+                    mail("ladhawala.nimit@gmail.com", "Message from Sewa Norway Website", $message, $headers);					
+					
+					mail("$_POST[email]", "Message from Sewa Norway Website", $message1, $headers);					
+
+					
+	}
+
+
+header('location:thanks.html');
+exit(0);
+
+
+?>
